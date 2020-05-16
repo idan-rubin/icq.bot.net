000b7c8d53681701314cb54ce3e4b26f4b17e111
Idan Rubin <rubin.idan@gmail.com>
2020-05-16 13:46:55 +0300

    adding more fields to CallbackQueryEventArgs

--- ICQ.Bot/Args/CallbackQueryEventArgs.cs
+++ ICQ.Bot/Args/CallbackQueryEventArgs.cs
@@ -7,15 +7,18 @@ namespace ICQ.Bot.Args
     {
         public string CallbackData { get; }
         public string QueryId { get; }
+        public Message Message { get; }
+        public string Text { get; }
+        public User From { get; }
+        public Chat Chat { get; }
         internal CallbackQueryEventArgs(Update update)
         {
             CallbackData = update.Payload.CallbackData;
             QueryId = update.Payload.QueryId;
-        }
-
-        internal CallbackQueryEventArgs(string callbackData)
-        {
-            CallbackData = callbackData;
+            Message = update.Payload.Message;
+            From = update.Payload.From;
+            Chat = update.Payload.Chat;
+            Text = update.Payload.Text;
         }
 
         public static implicit operator CallbackQueryEventArgs(UpdateEventArgs e) => new CallbackQueryEventArgs(e.Update);
